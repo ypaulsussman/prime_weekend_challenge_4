33e77e11247b4055c7e87614d7e2dc9d101e013b
Y Paul Sussman <ypaulsussman@gmail.com>
2017-03-31 16:30:52 -0500

    completes DOM append

--- client_dev/scripts/client.js
+++ client_dev/scripts/client.js
@@ -10,6 +10,23 @@ function getListings() {
   success: function(response) {
     console.log("successful call");
     console.log(response);
+    appendListings(response);
   }
 });
+}
+
+function appendListings(response) {
+  for (var i = 0; i < response.length; i++) {
+    $('.basket.container').append('<div class="row" id="property'+ i + '"></div>');
+    var $el = $('#property'+i);
+    // $('#propertyX')
+    $el.append('<div class="col-md-4"><p>'+response[i].sqft+' square feet!</p></div>');
+    $el.append('<div class="col-md-4"><p>Located in sunny '+response[i].city+'.</p></div>');
+    if (response[i].cost) {
+      $el.append('<div class="col-md-4"><p>Only $'+response[i].cost+' to own!</p></div>');
+    }else if (response[i].rent) {
+      $el.append('<div class="col-md-4"><p>Only $'+response[i].rent+' a month!</p></div>');
+    }
+    // response[i]
+  }
 }
