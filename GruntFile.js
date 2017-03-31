904836621c2e71011bd6a46fc29c143db003c817
Y Paul Sussman <ypaulsussman@gmail.com>
2017-03-31 15:36:34 -0500

    initializes grunt; builds client-side development directory

---
+++ GruntFile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  // require('load-grunt-tasks')(grunt);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      main: {
+        src: ["client_dev/scripts/client.js"],
+        dest: "server/public/scripts/client.min.js"
+      }
+    },//end uglify
+    copy: {
+      clientjs: {
+        expand: true,
+        cwd: 'client_dev/scripts',
+        src: ['*.js'],
+        dest: 'server/public/scripts'
+      },
+      html: {
+        expand: true,
+        cwd: 'client_dev/views',
+        src: ['*.html'],
+        dest: 'server/public/views'
+      },
+      css: {
+        expand: true,
+        cwd: 'client_dev/styles',
+        src: ['*.css'],
+        dest: 'server/public/styles'
+      }
+    },//end copy
+    watch: {
+      files: ['client_dev/scripts/*.js', 'client_dev/views/*.html', 'client_dev/styles/*.css'],
+      tasks: ['uglify', 'copy']
+    }//end watch
+  });//end grunt.initConfig
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['uglify', 'copy', 'watch']);
+};//end module.exports
